3ca1fd1a57eadd919945204a3e2cb03b84ab288d
focso5 <focso5@gmail.com>
2021-02-15 17:21:48 +0900

    HEX 배경색 변경 앱

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ const changeBg = document.querySelector('body');
 
 clickBtn.addEventListener('click', () => {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+  let hexValue = document.querySelector('#hex-value');
+  hexValue.innerText = randomColor;
+
   document.body.style.backgroundColor = '#' + randomColor;
   color.innerHTML = '#' + randomColor;
 });
